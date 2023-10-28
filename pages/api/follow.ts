f406d5b4d111729d9e1bfd974748e4068592f24d
Bruno Carvalho Feitosa <brunocarvalhofeitosa@outlook.com>
2023-10-28 10:17:05 -0300

    feat: change api calls

--- pages/api/follow.ts
+++ pages/api/follow.ts
@@ -9,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     const { userId } = req.body
-    const { currentUser } = await serverAuth(req, res)
+    const { currentUser } = await serverAuth(req)
 
     if (!userId || typeof userId !== "string") {
       throw new Error("Invalid ID.")
