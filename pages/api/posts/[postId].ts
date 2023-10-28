9bc0840e9b72e87b7944d55875dc3df7f283e84e
Bruno Carvalho Feitosa <brunocarvalhofeitosa@outlook.com>
2023-10-28 00:19:47 -0300

    feat: change posts api

--- pages/api/posts/[postId].ts
+++ pages/api/posts/[postId].ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import prisma from "@/libs/prismadb"
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "GET") {
-    return res.status(405).end()
-  }
-
+export default async function handler(req: NextApiRequest, res: NextApiResponse) { 
   try {
+    if (req.method !== "GET") {
+      return res.status(405).end()
+    }
+
     const { postId } = req.query
 
     if (!postId || typeof postId !== "string") {
