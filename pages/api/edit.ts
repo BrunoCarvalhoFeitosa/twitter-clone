bef8c260856852e8adfef76a3633b660caf5dfcd
Bruno Carvalho Feitosa <brunocarvalhofeitosa@outlook.com>
2023-10-27 23:52:38 -0300

    feat: change serverAuth rules

--- pages/api/edit.ts
+++ pages/api/edit.ts
@@ -3,16 +3,16 @@ import serverAuth from "@/libs/serverAuth"
 import prisma from "@/libs/prismadb"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'PATCH') {
-    return res.status(405).end()
-  }
-
   try {
+    if (req.method !== "PATCH") {
+      return res.status(405).end()
+    }
+
     const { currentUser } = await serverAuth(req, res)
     const { name, username, bio, profileImage, coverImage } = req.body
 
     if (!name || !username) {
-      throw new Error('Missing fields.')
+      throw new Error("Missing fields.")
     }
 
     const updatedUser = await prisma.user.update({
